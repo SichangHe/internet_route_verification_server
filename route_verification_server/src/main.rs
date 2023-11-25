0b92e94ae1cb9a557e06559d23ad8c263a454d27
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-11-25 20:26:12 +0800

    trivial postgres sqlx example

--- route_verification_server/src/main.rs
+++ route_verification_server/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:postgres@localhost/irv_server_test")
+        .await?;
+
+    const TEN: i64 = 10;
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(TEN)
+        .fetch_one(&pool)
+        .await?;
+
+    assert_eq!(row.0, TEN);
+
+    Ok(())
 }
