a2f6911507514b5ac3fc6caa59f63659f6c1b126
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-12-03 21:24:31 +0800

    get AS for overall type

--- flask_RestAPI/app.py
+++ flask_RestAPI/app.py
@@ -2,6 +2,56 @@ import psycopg
 from dotenv import load_dotenv
 from flask import Flask, jsonify, render_template
 
+OVERALL_REPORT_TYPES = ("ok", "skip", "unrecorded", "special_case", "bad")
+REPORT_ITEM_TYPES = (
+    "skip_regex_tilde",
+    "skip_regex_with_set",
+    "skip_community",
+    "unrec_import_empty",
+    "unrec_export_empty",
+    "unrec_filter_set",
+    "unrec_as_routes",
+    "unrec_route_set",
+    "unrec_as_set",
+    "unrec_as_set_route",
+    "unrec_some_as_set_route",
+    "unrec_aut_num",
+    "unrec_peering_set",
+    "spec_uphill",
+    "spec_uphill_tier1",
+    "spec_tier1_pair",
+    "spec_import_peer_oifps",
+    "spec_import_customer_oifps",
+    "spec_export_customers",
+    "spec_import_from_neighbor",
+    "spec_as_is_origin_but_no_route",
+    "spec_as_set_contains_origin_but_no_route",
+    "err_filter",
+    "err_filter_as_num",
+    "err_filter_as_set",
+    "err_filter_prefixes",
+    "err_filter_route_set",
+    "err_remote_as_num",
+    "err_remote_as_set",
+    "err_except_peering_right",
+    "err_peering",
+    "err_regex",
+    "rpsl_as_name",
+    "rpsl_filter",
+    "rpsl_regex",
+    "rpsl_unknown_filter",
+    "recursion",
+)
+
+
+def is_valid_overall_type(overall_type: str):
+    return overall_type in OVERALL_REPORT_TYPES
+
+
+def is_valid_report_item_type(report_item_type: str):
+    return report_item_type in REPORT_ITEM_TYPES
+
+
 load_dotenv()
 app = Flask(__name__)
 
@@ -30,7 +80,7 @@ def get_verification_reports(observed_route_id):
 
 
 @app.route("/aut_num/<int:as_num>", methods=["GET"])
-def get_as_info(as_num):
+def get_aut_num(as_num: int):
     return execute_one(
         """
 SELECT as_num, as_name, imports, exports FROM aut_num WHERE as_num = %s
@@ -39,13 +89,42 @@ SELECT as_num, as_name, imports, exports FROM aut_num WHERE as_num = %s
     )
 
 
+@app.route("/as_for_overall_type/<string:overall_type>", methods=["GET"])
+def get_as_for_overall_type(overall_type: str):
+    """ASes that have at least one report with the given overall_type and
+    the number of reports with that overall_type."""
+    if not is_valid_overall_type(overall_type):
+        return jsonify({"message": "Invalid overall_type"}), 400
+    with conn.cursor() as cur:
+        results = cur.execute(
+            """
+WITH filtered_exchange_report AS (
+    SELECT * FROM exchange_report WHERE overall_type = %s
+) SELECT aggregated_as.as_num, count(*) FROM (
+    (
+        SELECT report_id, from_as AS as_num
+        FROM filtered_exchange_report
+        ORDER BY as_num
+    ) UNION (
+        SELECT report_id, to_as AS as_num
+        FROM filtered_exchange_report
+        ORDER BY as_num
+    )
+) AS aggregated_as
+GROUP BY as_num
+ORDER BY as_num
+                """,
+            (overall_type,),
+        ).fetchall()
+    return jsonify(results)
+
+
 @app.route("/overall_type/<string:overall_type>", methods=["GET"])
 def get_by_overall_type(overall_type):
     with conn.cursor() as cur:
         reports = cur.execute(
             "SELECT * FROM exchange_report WHERE overall_type = %s", (overall_type,)
         ).fetchall()
-
     return jsonify(reports)
 
 
