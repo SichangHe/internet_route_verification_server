6f5959a520b43b2c23c8c0c683554039b4058872
voidreaming <2707786374wsj@gmail.com>
2023-12-03 09:51:19 +0800

    basic endpoint develop without test

--- flask_RestAPI/app.py
+++ flask_RestAPI/app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 conn = psycopg.connect(f"dbname=irv_server_test")
 
 
+
 @app.route("/rpsl_obj/<string:rpsl_obj_name>", methods=["GET"])
 def get_rpsl_obj_by_name(rpsl_obj_name):
     with conn.cursor() as cur:
@@ -17,6 +18,59 @@ def get_rpsl_obj_by_name(rpsl_obj_name):
     if entry:
         return jsonify(entry)
     return jsonify({"message": "Entry not found"}), 404
+
+@app.route("/verification_reports/<int:observed_route_id>", methods=["GET"])
+def get_verification_reports(observed_route_id):
+    with conn.cursor() as cur:
+        reports = cur.execute(
+            "SELECT * FROM exchange_report WHERE parent_observed_route = %s",
+            (observed_route_id,),
+        ).fetchall()
+    return jsonify(reports)
+
+@app.route("/as_info/<int:as_num>", methods=["GET"])
+def get_as_info(as_num):
+    with conn.cursor() as cur:
+        rpsl_objs = cur.execute(
+            "SELECT * FROM rpsl_obj WHERE as_num = %s", (as_num,)
+        ).fetchall()
+        routes = cur.execute(
+            "SELECT * FROM route_obj WHERE origin = %s", (as_num,)
+        ).fetchall()
+    return jsonify({"rpsl_objs": rpsl_objs, "routes": routes})
+
+@app.route("/overall_type/<string:overall_type>", methods=["GET"])
+def get_by_overall_type(overall_type):
+    with conn.cursor() as cur:
+        reports = cur.execute(
+            "SELECT * FROM exchange_report WHERE overall_type = %s", (overall_type,)
+        ).fetchall()
+
+    return jsonify(reports)
+
+@app.route("/overall_type/<string:overall_type>", methods=["GET"])
+def get_by_overall_type(overall_type):
+    with conn.cursor() as cur:
+        reports = cur.execute(
+            "SELECT * FROM exchange_report WHERE overall_type = %s", (overall_type,)
+        ).fetchall()
+    return jsonify(reports)
+
+@app.route("/overall_type/<string:overall_type>", methods=["GET"])
+def get_by_overall_type(overall_type):
+    with conn.cursor() as cur:
+        reports = cur.execute(
+            "SELECT * FROM exchange_report WHERE overall_type = %s", (overall_type,)
+        ).fetchall()
+    return jsonify(reports)
+
+
+
+
+
+
+
+
 
 
 @app.route("/")
--- restore_db.sh
+++ restore_db.sh
@@ -1,1 +1,6 @@
-psql irv_server_test < irv_server_dump.gz
+# export PGCLIENTENCODING=UTF8
+gunzip -c irv_server_dump.gz | psql -U postgres -d irv_server_test
+# gunzip -c irv_server_dump.gz | psql -U postgres -d irv_server_test -f -
+
+
+# psql -U postgres -d irv_server_test < irv_server_dump.gz
