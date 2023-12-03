998a4be61bbb157b8b25ef533941cd7f3b449017
voidreaming <2707786374wsj@gmail.com>
2023-12-03 15:27:07 +0800

    fix query bug

--- queries for restAPI endpoint.sql
+++ queries for restAPI endpoint.sql
@@ -1,54 +1,35 @@
 -- Query ASes, routes, reports, and report items for a given overall type
 -- the reports belong to with timestamps, item count, and paging.
-WITH result_set AS (
-    SELECT
-        e.from_as AS source_as,
-        e.to_as AS destination_as,
-        e.import,
-        e.overall_type,
-        e.recorded_time AS exchange_report_time,
-        r.observed_route_id,
-        r.raw_line,
-        r.address_prefix,
-        r.recorded_time AS observed_route_time,
-        ri.category AS report_category,
-        ri.specific_case AS report_specific_case,
-        ri.str_content AS report_string_content,
-        ri.num_content AS report_numeric_content,
-        COUNT(*) OVER () AS total_items -- Total item count
-    FROM
-        exchange_report e
-    JOIN
-        observed_route r ON e.parent_observed_route = r.observed_route_id
-    LEFT JOIN
-        report_item ri ON e.report_id = ri.parent_report
-    WHERE
-        e.overall_type = 'ok'
-)
--- Apply pagination
 SELECT
-    source_as,
-    destination_as,
-    import,
-    overall_type,
-    exchange_report_time,
-    observed_route_id,
-    raw_line,
-    address_prefix,
-    observed_route_time,
-    report_category,
-    report_specific_case,
-    report_string_content,
-    report_numeric_content,
-    total_items
+    e.from_as AS source_as,
+    e.to_as AS destination_as,
+    e.import,
+    e.overall_type,
+    e.recorded_time AS exchange_report_time,
+    r.observed_route_id,
+    r.raw_line,
+    r.address_prefix,
+    r.recorded_time AS observed_route_time,
+    ri.category AS report_category,
+    ri.specific_case AS report_specific_case,
+    ri.str_content AS report_string_content,
+    ri.num_content AS report_numeric_content,
+    COUNT(*) OVER () AS total_items
 FROM
-    result_set
+    exchange_report e
+JOIN
+    observed_route r ON e.parent_observed_route = r.observed_route_id
+LEFT JOIN
+    report_item ri ON e.report_id = ri.parent_report
+WHERE
+    e.overall_type = 'ok'
 ORDER BY
-    exchange_report.recorded_time
+    e.recorded_time
 OFFSET
     0
 LIMIT
-    10; 
+    10;
+
 	
 --Query ASes, routes, reports and report items for a given specific case
 --the report items belong to.
